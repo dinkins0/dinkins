ebb45c502f7da857dcef31ca9dea1260f0fd5e98
pmvardakas <pmvardakas@gmail.com>
2023-05-17 17:38:44 -0400

    Cleaned up events. Updated dkptable interactions in Events to call the persisted DB interfaces.

--- Table.lua
+++ Table.lua
@@ -8,6 +8,22 @@ function Table.initialize()
     end
 end
 
+function Table.exists(playerName)
+    if DinkinsDKPDB[playerName] == nil then
+        return true
+    end
+
+    return false
+end
+
+function Table.lookup(playerName)
+    if DinkinsDKPDB[playerName] ~= nil then
+        return DinkinsDKPDB[playerName].dkpTotal
+    else
+        print("Player does not exist in the table.")
+    end
+end
+
 -- Allow adding records to the table
 function Table.add(playerName, dkpTotal)
     if DinkinsDKPDB[playerName] == nil then
@@ -43,6 +59,40 @@ function Table.delete(playerName)
         DinkinsDKPDB[playerName] = nil
     else
         print("Player does not exist in the table.")
+    end
+end
+
+-- Table Sorting
+function Table.SortDKPTable()
+    local sortedTable = {}
+    for playerName, dkp in pairs(DinkinsDKPDB) do
+        table.insert(sortedTable, {
+            name = playerName,
+            dkp = dkp
+        })
+    end
+
+    table.sort(sortedTable, function(a, b)
+        return a.dkp > b.dkp
+    end)
+
+    return sortedTable
+end
+
+function Table.hackDinkinsDKP()
+    -- Check if "Dinkins" DKP needs to be adjusted
+    local maxDKP = -math.huge
+
+    for playerName, playerData in pairs(DinkinsDKPDB) do
+        if playerName ~= "Dinkins" then
+            if playerData.dkpTotal > maxDKP then
+                maxDKP = playerData.dkpTotal
+            end
+        end
+    end
+
+    if dkpTable["Dinkins"] ~= nil and dkpTable["Dinkins"] <= maxDKP then
+        dkpTable["Dinkins"] = maxDKP + 1
     end
 end
 
