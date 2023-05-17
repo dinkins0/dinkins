e10d603e706af6c9735d811c115dea559fd50c42
pmvardakas <pmvardakas@gmail.com>
2023-05-17 16:51:46 -0400

    Formatting cleanup. Persistence and interface for database.

--- Table.lua
+++ Table.lua
@@ -1,18 +1,49 @@
 -- Table.lua
-DinkinsDKPDB = DinkinsDKPDB or {}
-local table = DinkinsDKPDB.dkpTable or {}
+local Table = {}
 
-function table.initialize()
-    -- Define DKP storage table
-    DinkinsDKPDB.dkpTable = {}
-
-    -- Add Global variable to store the add-on's namespace
-    DinkinsDKPDB.DinkinsDKP = {}
+-- Initialize local table with either WoW Saved Variable or default
+function Table.initialize()
+    if DinkinsDKPDB == nil then
+        DinkinsDKPDB = {}
+    end
 end
 
--- Function to save the 'dkpTable' data
-function table.SaveData(dkpTable)
-    DinkinsDKPDB.dkpTable = dkpTable
+-- Allow adding records to the table
+function Table.add(playerName, dkpTotal)
+    if DinkinsDKPDB[playerName] == nil then
+        DinkinsDKPDB[playerName] = {
+            dkpTotal = dkpTotal
+        }
+    else
+        print("Player already exists in the table.")
+    end
 end
 
-return DinkinsDKPDB
+-- Allow modifying records in the table
+function Table.modify(playerName, dkpTotal)
+    if DinkinsDKPDB[playerName] ~= nil then
+        DinkinsDKPDB[playerName].dkpTotal = dkpTotal
+    else
+        print("Player does not exist in the table.")
+    end
+end
+
+-- Allow modifying records in the table by adding to the existing dkpTotal
+function Table.modifyByAdding(playerName, dkpToAdd)
+    if DinkinsDKPDB[playerName] ~= nil then
+        DinkinsDKPDB[playerName].dkpTotal = DinkinsDKPDB[playerName].dkpTotal + dkpToAdd
+    else
+        print("Player does not exist in the table.")
+    end
+end
+
+-- Allow deleting records from the table
+function Table.delete(playerName)
+    if DinkinsDKPDB[playerName] ~= nil then
+        DinkinsDKPDB[playerName] = nil
+    else
+        print("Player does not exist in the table.")
+    end
+end
+
+return Table
