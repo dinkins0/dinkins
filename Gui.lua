3aec5cf353ba90897941a215bbee0af44c6bdcdc
mbouren <62515796+mbouren@users.noreply.github.com>
2023-05-18 16:02:14 -0400

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -5,12 +5,12 @@ local addonFrame = CreateFrame("Frame", "DinkinsDKPAddonFrame", UIParent)
 addonFrame:SetSize(300, 400)
 addonFrame:SetPoint("CENTER")
 addonFrame:SetBackdrop({
-      bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
-      edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-      tile = true,
-      tileSize = 16,
-      edgeSize = 16,
-      insets = { left = 4, right = 4, top = 4, bottom = 4 },
+    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+    edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+    tile = true,
+    tileSize = 16,
+    edgeSize = 16,
+    insets = { left = 4, right = 4, top = 4, bottom = 4 },
 })
 addonFrame:SetBackdropColor(0, 0, 0, 0.8)
 addonFrame:SetMovable(true)
@@ -25,15 +25,14 @@ local soundFilePath = "Interface\\Addons\\DinkinsDKP\\questing.mp3"
 
 -- Function to play the sound
 local function PlaySoundOnShow()
-   PlaySoundFile(soundFilePath, "Master")
+    PlaySoundFile(soundFilePath, "Master")
 end
 
 -- Show the GUI
 local function ShowGUI()
-   addonFrame:Show()
-   PlaySoundOnShow()
+    addonFrame:Show()
+    PlaySoundOnShow()
 end
-
 
 -- Create the title
 local title = addonFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
@@ -41,10 +40,16 @@ title:SetPoint("TOP", 0, -16)
 title:SetText("DINKINS KINDNESS POINTS!!!")
 title:SetFont("Fonts\\ARIALN.TTF", 20, "OUTLINE")
 
+-- Create the image
+local image = addonFrame:CreateTexture(nil, "ARTWORK")
+image:SetTexture("Interface\\Addons\\DinkinsDKP\\your_image_file.png")
+image:SetPoint("TOP", addonFrame, "TOP", 0, -50)
+image:SetSize(addonFrame:GetWidth() - 40, 100)
+
 -- Create the leaderboard scroll frame
 local scrollFrame = CreateFrame("ScrollFrame", "DinkinsDKPScrollFrame", addonFrame, "UIPanelScrollFrameTemplate")
 scrollFrame:SetSize(250, 300)
-scrollFrame:SetPoint("TOP", title, "BOTTOM", 0, -16)
+scrollFrame:SetPoint("TOP", image, "BOTTOM", 0, -16)
 
 -- Create the leaderboard content
 local leaderboard = CreateFrame("Frame", "DinkinsDKPLeaderboard", scrollFrame)
@@ -57,59 +62,48 @@ local animationOffset = 5
 
 -- Populate the leaderboard with data from the dkpTable
 local dkpTable = {
-   { name = "Dinkins", dkp = 300, nameText = nil },
-   { name = "Theban", dkp = 200, nameText = nil },
-   { name = "Pneucrusader", dkp = 150, nameText = nil },
-   -- !!!! This can be removed and referenced with the actual DKP table once this is all strung up !!!
+    { name = "Dinkins", dkp = 300, nameText = nil },
+    { name = "Theban", dkp = 200, nameText = nil },
+    { name = "Pneucrusader", dkp = 150, nameText = nil },
+    -- !!!! This can be removed and referenced with the actual DKP table once this is all strung up !!!
 }
 
 local yOffset = 0
 local rowHeight = 20
 for i, data in ipairs(dkpTable) do
-   local nameText = leaderboard:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-   nameText:SetPoint("TOPLEFT", 8, -yOffset)
-   nameText:SetText(data.name)
-   data.nameText = nameText  -- Store the nameText in the data table
-   
-   local dkpText = leaderboard:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-   dkpText:SetPoint("TOPRIGHT", -8, -yOffset)
-   dkpText:SetText(data.dkp)
-   
-   yOffset = yOffset + rowHeight
+    local nameText = leaderboard:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    nameText:SetPoint("TOPLEFT", 8, -yOffset)
+    nameText:SetText(data.name)
+    data.nameText = nameText  -- Store the nameText in the data table
+
+    local dkpText = leaderboard:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    dkpText:SetPoint("TOPRIGHT", -8, -yOffset)
+    dkpText:SetText(data.dkp)
+
+    yOffset = yOffset + rowHeight
 end
 
 scrollFrame:SetScrollChild(leaderboard)
 
--- Register slash commands to show/hide the GUI Note: Commented out. Redundant I believe when called in DinkinsDKP.lua
--- SLASH_DKPGUI1 = "/dkp"
--- SLASH_DKPGUI2 = "/dinkinsdkp"
--- SlashCmdList["DKPGUI"] = function()
---    if addonFrame:IsShown() then
---        addonFrame:Hide()
---    else
---        addonFrame:Show()
---    end
---end
-
 -- Start the animation
 local originalY = {}
 local function StartAnimation()
-   for i, data in ipairs(dkpTable) do
-      originalY[i] = data.nameText:GetPoint(2)
-   end
-   
-   addonFrame:SetScript("OnUpdate", function(self, elapsed)
-         animationTimer = animationTimer + elapsed
-         for i, data in ipairs(dkpTable) do
+    for i, data in ipairs(dkpTable) do
+        originalY[i] = data.nameText:GetPoint(2)
+    end
+
+    addonFrame:SetScript("OnUpdate", function(self, elapsed)
+        animationTimer = animationTimer + elapsed
+        for i, data in ipairs(dkpTable) do
             local yOffset = originalY[i] + math.sin(animationTimer * animationSpeed) * animationOffset
             data.nameText:SetPoint("TOPLEFT", 8, -yOffset)
-         end
-   end)
+        end
+    end)
 end
 
 -- Stop the animation
 local function StopAnimation()
-   addonFrame:SetScript("OnUpdate", nil)
+    addonFrame:SetScript("OnUpdate", nil)
 end
 
 -- Hook the show/hide events to start/stop the animation
